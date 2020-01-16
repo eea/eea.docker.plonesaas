c86eb547e02efc307e093b8402346b634980591a
Alin Voinea <contact@avoinea.com>
2020-01-16 18:15:49 +0200

    Refs #112513 - Create plonesaas-devel

---
+++ devel/docker-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+runDeps="
+  build-essential
+  silversearcher-ag
+  tmux
+  mc
+  procps
+"
+
+echo "========================================================================="
+echo "Installing $runDeps"
+echo "========================================================================="
+
+apt-get update
+apt-get install -y --no-install-recommends $runDeps
+
+echo "========================================================================="
+echo "Running buildout -c develop.cfg"
+echo "========================================================================="
+
+buildout -c develop.cfg
+
+echo "========================================================================="
+echo "Cleaning up cache..."
+echo "========================================================================="
+
+rm -vrf /var/lib/apt/lists/*
+rm -vrf /plone/buildout-cache/downloads/*
+
+echo "========================================================================="
+echo "Fixing permissions..."
+echo "========================================================================="
+
+find /data  -not -user plone -exec chown plone:plone {} \+
+find /plone -not -user plone -exec chown plone:plone {} \+
