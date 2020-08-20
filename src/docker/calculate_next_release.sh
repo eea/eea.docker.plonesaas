d70f0ebf04919a5c2dba4f8a4dd9745fa01b00b6
valentinab25 <valentinab25>
2020-08-20 18:16:02 +0300

    Fix case with multiple releases per commit

--- src/docker/calculate_next_release.sh
+++ src/docker/calculate_next_release.sh
@@ -1,1 +1,1 @@
-major=$(grep FROM Dockerfile | awk -F':|-' '{print $2}');let minor=$(git describe --tags --abbrev=0 | grep -E $major-[0-9]+ | cut -d- -f2 )+1; echo $major-$minor
+major=$(grep FROM Dockerfile | awk -F':|-' '{print $2}');let minor=$(git tag | grep -E $major-[0-9]+ | cut -d- -f2 | sort -n | tail -n 1 )+1; echo $major-$minor
